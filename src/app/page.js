7e5f7ace3019ba86612c1922c45b9fefc79f8ca8
shashikant kumar bind <162382147+stechbindra2@users.noreply.github.com>
2026-06-26 06:48:44 +0530

    Integrate AlphaMarquee component into main page

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@
 import Hero from '@/components/Hero';
 import Services from '@/components/Services';
 import Footer from '@/components/Footer';
+import AlphaMarquee from '@/components/AlphaMarquee';
 
 import React from 'react';
 
 const Page = () => {
   return (
     <>
+    <AlphaMarquee />
     <div className='min-h-[100vh] w-[100hw]'>
       <Hero/>
       <Services/>
